283264676edb2659274098bb125b7b8eb600a459
napoxog <tobex@yandex.ru>
2018-08-01 20:23:01 +0300

    lots of fixing

--- app.R
+++ app.R
@@ -10,12 +10,16 @@
 library(shiny)
 library(readr)
 library(DT)
+library(stats)
 # INIT: lists ####
 parNames = list("Накопленная добыча газа",
                     "Накопленная добыча воды",
                     "Пластовое давление",
                     "Устьевое давление")
 names(parNames) = c ( "WGPT","WWPT","WBP9","WTHP" )
+
+tableTypes = list('yearly','platform')
+names(tableTypes) = c('годичная','по кустам')
 
 dayNames = c(1:31)
 monNames = c(1:12)
@@ -78,24 +82,109 @@ ui <- fluidPage(
       # Show a plot of the generated distribution
       mainPanel(
          #plotOutput("distPlot"),
+        radioButtons(inputId = 'tableMode','Отобразить:',choices = tableTypes,inline = T),
         dataTableOutput(outputId = 'dataOutput'),
-         actionButton("asd","asd")
+        downloadButton('downloadSummary', 'Сохранить в файл')
       )
    )
 )
 
+# Debug information print with call info
+dbgmes <- function (message = "message", expr = NULL, depth = 1) {
+  #browser()
+  calls = sys.calls()
+  depth=min(depth,length(calls))
+  range = c(length(calls)-depth:length(calls))
+  funame = unlist(strsplit(x = as.character(calls[range]),split = "\\("))
+  cat(paste0('==>',funame[1],':\n'))
+  cat(paste0(message,':',capture.output(expr),'\n'))
+}
+
+sumPart <- function (x,y) {
+  #dbgmes('y=',y)
+  return(sum(x,na.rm = T))
+}
+
+getNwells <- function (x) {
+  #browser()
+  #dbgmes("x=",x)
+  return(as.integer(length(x[!is.na(x) & x>0])) )
+}
+
+getSum <- function(x) {
+  #dbgmes("x=",x)
+  if(class(x)=='numeric')
+    res = sum(x,na.rm = T)
+  else res = NA
+  #browser()
+  #dbgmes("res=",res)
+  return(res)
+}
+
+getNA <- function(x) {
+  return(NA)
+}
+
+
+filterByPlatform <- function (data = NULL) {
+  if(is.null(data)) return(NULL)
+  scalingPars = c("WGPT","WWPT")
+  factNames=colnames(data)[c(1:3)]
+  colnames(data)[c(1:3)] = c('par','well','platf')
+  platforms = as.numeric(unique(data$'куст'))
+  dbgmes("platforms=",platforms)
+  procCols = colnames(data)[c(-1:-3)]
+  #browser()
+  id_prod = grep(x = data$par,pattern = "добыча")
+  id_pres = grep(x = data$par,pattern = "давление")
+  id_strt = grep(x = data$par,pattern = "добыча газа")
+  
+  prod_data = data[id_prod,]
+  filterList = list( unlist(prod_data[,3]),unlist(prod_data[,1]))
+  prod_sum = aggregate(x = prod_data[,procCols],by = filterList, FUN = getSum)[,c(-1:-2)]
+  factors = aggregate(x = prod_data[,c(3,1)],by = filterList, FUN = getNA)[,c(1:2)]
+  prod_sum = cbind(factors,prod_sum)
+  
+  prod_data = data[id_pres,]
+  filterList = list( unlist(prod_data[,3]),unlist(prod_data[,1]))
+  pres_avg = aggregate(x = prod_data[,procCols],by = filterList, FUN = mean, na.rm = T)[,c(-1:-2)]
+  factors = aggregate(x = prod_data[,c(3,1)],by = filterList, FUN = getNA)[,c(1:2)]
+  pres_avg = cbind(factors,pres_avg)
+  
+  prod_data = data[id_strt,]
+  filterList = list( unlist(prod_data[,3]),unlist(prod_data[,1]))
+  prod_col = aggregate(x = prod_data[,procCols],by = filterList, FUN = getNwells)[,c(-1:-2)]
+  prod_col = t(diff(rbind(0,t(prod_col))))
+  factors = aggregate(x = prod_data[,c(3,1)],by = filterList, FUN = getNA)[,c(1:2)]
+  prod_col = cbind(factors,prod_col)
+        
+  #browser()
+  prod_col$Group.2 = rep('ввод скважин',times = nrow(prod_col))
+  prod_sum$Group.2 = gsub('Накопленная д','Д', prod_sum$Group.2) 
+  fdf = rbind(prod_sum,pres_avg,prod_col)
+  colnames(fdf)[1:2] = c('куст','параметр')
+  return(fdf)  
+}
+  
 getFilteredData <- function(data=NULL,day=1,mon=1,pars = names(parNames)) {
   if(is.null(data)) return(NULL)
   if(is.null(pars) || length(pars)<1) pars = names(parNames)
-  scalingPars = c("WGPT","WWPT")
+  scalingPars = c("WGPT")
+  diffPars = c("WGPT","WWPT")
   #dataProduction[1,] = 0
   #browser()
   #DP = data[,-1]
   #browser()
+  colnames = strsplit(x = names(data[-1]),':')
+  #params =  sapply(colnames,function(x) parNames[[x[1]]])
+  params =  sapply(colnames,function(x) parNames[[x[1]]])
+  wells =  sapply(colnames,function(x) x[2])
+  platforms = sapply(wells,getPlatformName)
   years = sapply(data$DATE,function(x) as.numeric(format(x, "%Y")))
   days = sapply(data$DATE,function(x) as.numeric(format(x, "%d")))
   mons = sapply(data$DATE,function(x) as.numeric(format(x, "%m")))
   DP = cbind(data,years,mons,days)
+  #browser()
   #yearlyData[,!names(yearlyData) %in% c('years','mons','days')]
   procNames = names(DP)[(!names(DP) %in% c('years','mons','days','DATE'))]
   #procNames = names(DP)[(!procNames %in% c('years','mons','days','DATE'))]
@@ -111,24 +200,31 @@ getFilteredData <- function(data=NULL,day=1,mon=1,pars = names(parNames)) {
   #getParName(procNames[1])
   reducedData = sapply(procNames,function(x) { 
     par = strsplit(x,':')[[1]][1] 
-    if(par %in% scalingPars) reducedData[,x]*10^-9 
-    else reducedData[,x]
+    res = reducedData[,x]
+    if(par %in% scalingPars){
+      res = res*10^-9
+    }
+    if(par %in% diffPars){
+      res = c(0,diff(res))
+    }
+    
+    return(res)
     })
-  #browser()
-  reducedData = cbind(years = reducedYears,
-                      sapply(procNames,function(x) diff(reducedData[,x]) ))
+  reducedData = cbind(years = reducedYears,reducedData)
+  #                    sapply(procNames,function(x) diff(reducedData[,x]) ))
   reducedData = data.frame(reducedData)
   # for (i in 1:length(names(reducedData))) 
   # {
   #   q = diff(as.matrix(reducedData[,i])); 
   #   reducedData[,i] = c(0,q)
   # }
-  #browser()
+  browser()
   cnames = reducedData$years[-1]
   rnames = names(reducedData)[-1]
   reducedData = t(reducedData[-1,-1])
-  colnames(reducedData) = cnames
-  rownames(reducedData) = rnames
+  #rownames(reducedData) = rnames
+  reducedData=data.frame(params,wells,platforms,reducedData)
+  colnames(reducedData) = c('Параметр','скважина','куст',cnames)
   return(reducedData)
 }
 
@@ -138,6 +234,10 @@ getPlatformName <- function (x = NULL) {
     else return(NA)
 }  
 # Define server logic required to draw a histogram
+
+options(shiny.encoding = "UTF-8")
+options(encoding = "UTF-8")
+
 server <- function(input, output,session) {
    
    output$distPlot <- renderPlot({
@@ -150,10 +250,23 @@ server <- function(input, output,session) {
    })
    
    output$dataOutput <- renderDataTable({
-     datatable(data = myReactives$FilteredData,
+     widetarget = 0
+     if(input$tableMode == 'yearly'){
+       data = myReactives$FilteredData
+       widetarget = 0
+       datacols = colnames(data)[c(-1:-3)]
+     }
+     else if(input$tableMode == 'platform') {
+       data = myReactives$FilteredByPlatform#filterByPlatform(myReactives$FilteredData)
+       widetarget = 1
+       datacols = colnames(data)[c(-1:-2)]
+     }
+     
+     if(is.null(data) || nrow(data)<2) return(NULL)
+     datatable(data = data,
                height = 30,
                class = "compact",
-               rownames = TRUE,
+               rownames = FALSE,
                selection = list (
                  mode = 'multiple',
                  #      selection  = sr,
@@ -162,12 +275,14 @@ server <- function(input, output,session) {
                options = list(
                  pagingType = "simple",
                  paging = FALSE,
-                 ColumnRender = prettyNum,
+                 #ColumnRender = prettyNum,
                  scrollY = "500px",
                  scrollX = TRUE,#"800px",
                  scrollCollapse = TRUE,
                  #      stateSave = TRUE,
-                 pageLength = 30#,
+                 pageLength = 30,
+                 autoWidth = TRUE,
+                 columnDefs = list(list(width = '200px', targets = widetarget))#,
                  # columnDefs = list(
                  #   list(visible = FALSE, targets = c(2:3)),
                  #   list(orderable = FALSE, className = 'details-control', targets = 0)
@@ -191,7 +306,9 @@ server <- function(input, output,session) {
                #               });"
                # 
                # )
-     ) #%>% formatDate(table = myReactives$data,columns =  1, method = "toLocaleDateString")
+     ) #%>%  formatRound(columns = c(4:ncol(data)),digits = 5)#  %>% formatStyle(columns = c(widetarget), width='200px') 
+     
+     #%>% formatDate(table = myReactives$data,columns =  1, method = "toLocaleDateString")
    })
    
    output$wells <-renderDataTable({
@@ -218,6 +335,30 @@ server <- function(input, output,session) {
      )
      })
    
+   output$platforms <-renderDataTable({
+     datatable(myReactives$platforms,
+               height = 15,
+               class = "compact",
+               rownames = FALSE,
+               filter = 'none',
+               editable = F,
+               options = list(
+                 pagingType = "simple",
+                 paging = FALSE,
+                 ColumnRender = prettyNum,
+                 scrollY = "400px",
+                 #scrollX = "800px",
+                 scrollCollapse = TRUE,
+                 #      stateSave = TRUE,
+                 pageLength = 30#,
+                 # columnDefs = list(
+                 #   list(visible = FALSE, targets = c(2:3)),
+                 #   list(orderable = FALSE, className = 'details-control', targets = 0)
+                 # )
+               )#,
+     )
+   })
+
    output$pars <-renderDataTable({
      #browser()
      datatable(myReactives$pars,
@@ -294,7 +435,7 @@ DATE  PAR:WELL  PAR:WELL ...",
                                                    day = input$startDay,
                                                    mon=input$startMon,
                                                    pars = names(parNames[input$pars_rows_selected]))
-       
+       myReactives$FilteredByPlatform = filterByPlatform(myReactives$FilteredData)
        #myReactives$data = reducedData
        #updateDateInput(session = session,
        #                inputId = input$startDate,
@@ -303,7 +444,29 @@ DATE  PAR:WELL  PAR:WELL ...",
        #names(dataProduction)
      }
    })
+   getSaveFilename <- function() {
+     fn = paste0('ProductionSummary_',basename(input$openDataFile$datapath),'-',Sys.Date(), '.txt')
+     return(fn)
    }
+   getSaveContent <- function(fname) {
+     browser()
+     if(input$tableMode == 'yearly') 
+       data = myReactives$FilteredData
+     else if(input$tableMode == 'platform') 
+       data = myReactives$FilteredByPlatform#filterByPlatform(myReactives$FilteredData)
+     #write_delim(data,path = fname,delim = '\t',na = 0,col_names = T)
+     #Encoding(colnames(data)) <- "UTF-8"
+     #Encoding(rownames(data)) <- "UTF-8"
+     #Encoding(data[2]) <- "UTF-8"
+     write.csv(x = data,file = fname,row.names = F,fileEncoding = "UTF-8",encodeing = "UTF-8")
+   }
+   
+   output$downloadSummary <- downloadHandler(
+     filename = getSaveFilename,
+     contentType = '.asc',
+     content = getSaveContent
+   )
+}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
